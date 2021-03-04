59452c7be20907e849092a0e1a98f1165cb85766
Pradum Agrawal <pradhumagrawal85@gmail.com>
2021-03-04 12:31:12 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import 'bootswatch/dist/materia/bootstrap.min.css';
+import Search from "./Search"
+
+function App() {
+  return (
+    <div className="container">
+     <Search/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Images.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Images extends React.Component{
+
+    render(){
+            const { image } = this.props;
+            return(
+                
+                    <div className="col-md-3">
+                        <img src={image.src.large} alt="Not Found" 
+                        className="img-fluid" />
+                    </div>
+                
+            )
+        }
+    
+}
+
+export default Images;
